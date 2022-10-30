5105d7c87dd1255b41bc91645a26adb1ba2601bb
manami-project <13179375+manami-project@users.noreply.github.com>
2022-10-30 17:37:42 +0100

    Created suspend version of tempDirectory()

--- src/main/kotlin/io/github/manamiproject/modb/test/TempDirectory.kt
+++ src/main/kotlin/io/github/manamiproject/modb/test/TempDirectory.kt
@@ -1,5 +1,6 @@
 package io.github.manamiproject.modb.test
 
+import kotlinx.coroutines.runBlocking
 import java.io.Closeable
 import java.nio.file.Files
 import java.nio.file.Path
@@ -32,10 +33,31 @@ public class TempDirectory(prefix: String = "modb_") : Closeable {
  *
  * @since 1.0.0
  */
-public fun tempDirectory(action: TempDirectory.() -> Unit) {
+@Deprecated("Use coroutine instead", ReplaceWith(""))
+public fun tempDirectory(action: TempDirectory.() -> Unit): Unit = tempDirectorySuspendable { action.invoke(this) }
+
+/**
+ * Creates a temporary directory and ensures that it is removed after test execution even if an exception occurred.
+ * The temporary directory is prefixed with _modb__.
+ * You can access the created temporary directry within [action] using the variable `tempDir`.
+ *
+ * **Example**
+ *
+ * ```
+ * tempDirectorySuspendable {
+ *      println(Files.exist(tempDir))
+ * }
+ * ```
+ *
+ * @since 1.4.0
+ * @param action Can take any action including suspend functions.
+ */
+public fun tempDirectorySuspendable(action: suspend TempDirectory.() -> Unit) {
     val tempDir = TempDirectory()
 
     tempDir.use {
-        it.action()
+        runBlocking {
+            it.action()
+        }
     }
 }
